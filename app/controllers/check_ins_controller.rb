d4725e9779139f7f2e8c48ccb6030c429443b085
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-09-22 15:55:06 -0400

    Add ability to delete a check in

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,11 +1,10 @@
 class CheckInsController < ApplicationController
+  before_action :find_check_in, only: [:show, :destroy]
   def index
     @check_ins = CheckIn.all
   end
 
-  def show
-    @check_in = CheckIn.find(params[:id])
-  end
+  def show; end
 
   def new
     @check_in = CheckIn.new
@@ -22,7 +21,17 @@ class CheckInsController < ApplicationController
     end
   end
 
+  def destroy
+    @check_in.destroy!
+
+    redirect_to check_ins_path
+  end
+
 private
+
+  def find_check_in
+    @check_in = CheckIn.find(params[:id])
+  end
 
   def check_in_params
     params.require(:check_in).permit(:name, :role_happiness, :company_happiness, :comment, :emotion)
--- notes.md
+++ notes.md
@@ -2,5 +2,5 @@ Creating a New Check-In
 - [x] Routes (new and create)
 - [x] Controller actions (create and new)
 - [x] Views (new)
-- [ ] Validations in the model (:role_happiness and :company_happiness between 1 and 5)
+- [x] Validations in the model (:role_happiness and :company_happiness between 1 and 5)
 - [x] Button to get to the new form
