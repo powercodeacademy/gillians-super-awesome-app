ccf0efbccce2459cddf7c661688bf3b8259b8745
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-09-22 13:35:30 -0400

    Create app

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  CheckIn.create!({
+    name: Faker::Name.name,
+    role_happiness: rand(1..5),
+    company_happiness: rand(1..5),
+    emotion: Faker::Emotion.adjective,
+    comment: Faker::Lorem.paragraphs(number: 1)
+  })
+end
