8ca1628683cf3e73fac13fc10c3824b9e5a239fe
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-09-22 15:45:36 -0400

    Add validations

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -1,2 +1,4 @@
 class CheckIn < ApplicationRecord
+  validates :role_happiness, inclusion: { in: 0..5, message: "%{value} must be between 1 and 5." }
+  validates :company_happiness, inclusion: { in: 0..5, message: "%{value} must be between 1 and 5." }
 end
